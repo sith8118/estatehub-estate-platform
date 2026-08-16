9f9e4ed4c4348205a7c5d42c72986111840ad7d8
sith8118 <sithmikavindi2@gmail.com>
2026-08-16 14:03:42 +0530

    Initialize auth service and user model

---
+++ auth-servicenew/src/test/java/com/estatehub/auth_servicenew/AuthServicenewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estatehub.auth_servicenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServicenewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
