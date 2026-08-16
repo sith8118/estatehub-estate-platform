46a6e8a4b489817e9081ee4b6c7c95a40ce1ed1f
Ashyy02 <asheradasanayaka28@gmail.com>
2026-08-16 19:51:20 +0530

    Restore Property Service

---
+++ property-service/property-service/src/main/java/com/estatehub/property_service/PropertyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.estatehub.property_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyServiceApplication.class, args);
+	}
+
+}
